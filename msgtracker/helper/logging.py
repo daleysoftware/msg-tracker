be0ff948556c5e3518f16f0ec7fc5bff9519b2c0
Matt Pillar <matt@aerofs.com>
2015-12-04 10:15:22 -0800

    Collector service logs in GMT.

--- msgtracker/helper/logging.py
+++ msgtracker/helper/logging.py
@@ -1,7 +1,9 @@
 import logging
+import time
 
 
 def init():
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(levelname)-5.5s %(asctime)s %(message)s')
+    logging.Formatter.converter = time.gmtime
